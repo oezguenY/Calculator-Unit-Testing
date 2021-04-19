b640ef56c970bb4f36114052ce0936fdf6be70a7
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-04-19 14:10:42 +0200

    Finished testing the CalculatorViewController

--- wallcalcaroniTests/CalculatorViewControllerTests.swift
+++ wallcalcaroniTests/CalculatorViewControllerTests.swift
@@ -30,7 +30,30 @@ class CalculatorViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.valueLabel.text, "25")
     }
     
+    func testEqualsButton_ShouldPerformCalculationAndUpdateLabel() {
+        sut.twoBtn.sendActions(for: .touchUpInside)
+        sut.fiveBtn.sendActions(for: .touchUpInside)
+        sut.subtractBtn.sendActions(for: .touchUpInside)
+        sut.fiveBtn.sendActions(for: .touchUpInside)
+        sut.equalsBtn.sendActions(for: .touchUpInside)
+        XCTAssertEqual(sut.valueLabel.text, "20")
+    }
     
+    func testOperandButtons_ShouldChangeOperandWhenTapped() {
+        sut.fourBtn.sendActions(for: .touchUpInside)
+        sut.multiplyBtn.sendActions(for: .touchUpInside)
+        sut.addBtn.sendActions(for: .touchUpInside)
+        sut.fourBtn.sendActions(for: .touchUpInside)
+        sut.equalsBtn.sendActions(for: .touchUpInside)
+        XCTAssertEqual(sut.valueLabel.text, "8")
+    }
+    
+    func testClearButton_ShouldClearLabel() {
+        sut.eightBtn.sendActions(for: .touchUpInside)
+        sut.sevenBtn.sendActions(for: .touchUpInside)
+        sut.acBtn.sendActions(for: .touchUpInside)
+        XCTAssertEqual(sut.valueLabel.text, "0")
+    }
 
 
 }
